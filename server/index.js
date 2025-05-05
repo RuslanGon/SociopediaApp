a767d3e6b6dfef7e5411481e25301013f01728b7
RuslanGon <ruslan441006@gmail.com>
2025-05-05 13:09:39 +0300

    wer

---
+++ server/index.js
@@ -0,0 +1,19 @@
+import express from 'express'
+import bodyParser, { json } from 'body-parser'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import multer from 'multer'
+import helmet from 'helmet'
+import morgan from 'morgan'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+dotenv.config()
+app.use(express.json())
+app.use(helmet())
+app.use(helmet.crossOriginResourcePolicy({policy}))
+
+const app = express()
