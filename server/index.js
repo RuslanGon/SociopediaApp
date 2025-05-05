cb047356c7d8693d634f3713f7a8327455f659e2
RuslanGon <ruslan441006@gmail.com>
2025-05-05 13:24:18 +0300

    wer

--- server/index.js
+++ server/index.js
@@ -9,11 +9,30 @@ import morgan from 'morgan'
 import path from 'path'
 import { fileURLToPath } from 'url'
 
+// middleware
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 dotenv.config()
 app.use(express.json())
 app.use(helmet())
-app.use(helmet.crossOriginResourcePolicy({policy}))
+app.use(helmet.crossOriginResourcePolicy({policy: 'cross-origin'}))
+app.use(morgan('common'))
+app.use(bodyParser.json({limit: '30mb', extended: true}))
+app.use(bodyParser.urlencoded({limit: '30mb', extended: true}))
+app.use(cors())
+app.use('/assets', express.static(path.join(__dirname, 'public/assets')))
+
+// file storage
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'public/assets')
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname)
+
+    }
+})
+const upload = multer({ storage })
+
 
 const app = express()
