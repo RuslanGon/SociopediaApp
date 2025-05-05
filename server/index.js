9497de726dc9a14a613ce2b0d0afeea97bc6411d
RuslanGon <ruslan441006@gmail.com>
2025-05-05 13:25:13 +0300

    wer

--- server/index.js
+++ server/index.js
@@ -1,38 +1,42 @@
-import express from 'express'
-import bodyParser, { json } from 'body-parser'
-import mongoose from 'mongoose'
-import cors from 'cors'
-import dotenv from 'dotenv'
-import multer from 'multer'
-import helmet from 'helmet'
-import morgan from 'morgan'
-import path from 'path'
-import { fileURLToPath } from 'url'
+// Импорты
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import multer from 'multer';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-// middleware
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-dotenv.config()
-app.use(express.json())
-app.use(helmet())
-app.use(helmet.crossOriginResourcePolicy({policy: 'cross-origin'}))
-app.use(morgan('common'))
-app.use(bodyParser.json({limit: '30mb', extended: true}))
-app.use(bodyParser.urlencoded({limit: '30mb', extended: true}))
-app.use(cors())
-app.use('/assets', express.static(path.join(__dirname, 'public/assets')))
+// Константы
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-// file storage
+// Инициализация
+dotenv.config();
+const app = express(); 
+
+// Middleware
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
+app.use(morgan('common'));
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors());
+app.use('/assets', express.static(path.join(__dirname, 'public/assets')));
+
+// Multer storage
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, 'public/assets')
-    },
-    filename: function (req, file, cb) {
-        cb(null, file.originalname)
+  destination: function (req, file, cb) {
+    cb(null, 'public/assets');
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  }
+});
+const upload = multer({ storage });
 
-    }
-})
-const upload = multer({ storage })
-
-
-const app = express()
+// Дальше — роуты, запуск сервера и т.д.
