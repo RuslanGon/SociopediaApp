5f85ca47a886f7f5ad195fb42659dd323135eb50
RuslanGon <ruslan441006@gmail.com>
2025-05-06 15:48:22 +0300

    wer

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,5 +1,5 @@
 import UserModel from '../models/Users.js'
-
+import mongoose from 'mongoose';
 
 export const getUser = async (req, res) => {
     try {
@@ -16,18 +16,62 @@ export const getUser = async (req, res) => {
   };
   
 
-export const getUserFriends = async (req, res) => {
+  export const getUserFriends = async (req, res) => {
     try {
-        
+      const { id } = req.params; 
+  
+      const user = await UserModel.findById(id); 
+      if (!user) {
+        return res.status(404).json({ message: 'Пользователь не найден' });
+      }
+  
+      const friends = user.friends; 
+      res.status(200).json(friends); 
+  
     } catch (error) {
-        console.log(error);
+      console.error('Ошибка получения друзей:', error.message);
+      res.status(500).json({ message: 'Ошибка сервера' });
     }
-}
+  };
 
-export const addRemoveFriend = async (req, res) => {
+  export const addRemoveFriend = async (req, res) => {
     try {
-        
+      const { id, friendsId } = req.params;
+  
+      // Проверка на валидный ObjectId
+      if (!mongoose.Types.ObjectId.isValid(id) || !mongoose.Types.ObjectId.isValid(friendsId)) {
+        return res.status(400).json({ message: 'Некорректный ID пользователя или друга' });
+      }
+  
+      const user = await UserModel.findById(id);
+      const friend = await UserModel.findById(friendsId);
+  
+      if (!user || !friend) {
+        return res.status(404).json({ message: 'Пользователь или друг не найден' });
+      }
+  
+      const isFriend = user.friends.includes(friendsId);
+  
+      if (isFriend) {
+        // Удаляем друга у обоих
+        user.friends = user.friends.filter(fId => fId.toString() !== friendsId);
+        friend.friends = friend.friends.filter(fId => fId.toString() !== id);
+      } else {
+        // Добавляем друга обоим
+        user.friends.push(friendsId);
+        friend.friends.push(id);
+      }
+  
+      await user.save();
+      await friend.save();
+  
+      // Получаем список друзей с их данными
+      const friends = await UserModel.find({ _id: { $in: user.friends } });
+  
+      res.status(200).json(friends);
+  
     } catch (error) {
-        console.log(error);
+      console.error('Ошибка добавления/удаления друга:', error.message);
+      res.status(500).json({ message: 'Ошибка сервера' });
     }
-}
+  };
