c8ac13053a9a1423d7c57cd2913b0f5f58a02728
RuslanGon <ruslan441006@gmail.com>
2025-05-07 12:33:21 +0300

    were

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -85,5 +85,79 @@ import mongoose from 'mongoose';
       res.status(200).json(user)
     } catch (error) {
       console.log(error);
+      res.status(404).json({ message: 'Ошибка сервера' });
     }
   }
+
+  export const getUserFriends = async (req, res) => {
+    try {
+      const { id } = req.params;
+  
+      const user = await UserModel.findById(id);
+      if (!user) {
+        return res.status(404).json({ message: 'Пользователь не найден' });
+      }
+  
+      const friends = await Promise.all(
+        user.friends.map(friendId => UserModel.findById(friendId))
+      );
+  
+      const formattedFriends = friends.map(friend => ({
+        _id: friend._id,
+        firstName: friend.firstName,
+        lastName: friend.lastName,
+        picturePath: friend.picturePath,
+        location: friend.location,
+        occupation: friend.occupation,
+      }));
+  
+      res.status(200).json(formattedFriends);
+    } catch (error) {
+      console.error('Ошибка получения друзей:', error.message);
+      res.status(500).json({ message: 'Ошибка сервера' });
+    }
+  };
+
+  export const addRemoveFriend = async (req, res) => {
+    try {
+      const { id, friendId } = req.params;
+  
+      const user = await UserModel.findById(id);
+      const friend = await UserModel.findById(friendId);
+  
+      if (!user || !friend) {
+        return res.status(404).json({ message: 'Пользователь не найден' });
+      }
+  
+      const isFriend = user.friends.includes(friendId);
+  
+      if (isFriend) {
+        user.friends = user.friends.filter(friend => friend.toString() !== friendId);
+        friend.friends = friend.friends.filter(friend => friend.toString() !== id);
+      } else {
+        user.friends.push(friendId);
+        friend.friends.push(id);
+      }
+  
+      await user.save();
+      await friend.save();
+  
+      const friends = await Promise.all(
+        user.friends.map(friendId => UserModel.findById(friendId))
+      );
+  
+      const formattedFriends = friends.map(friend => ({
+        _id: friend._id,
+        firstName: friend.firstName,
+        lastName: friend.lastName,
+        picturePath: friend.picturePath,
+        location: friend.location,
+        occupation: friend.occupation,
+      }));
+  
+      res.status(200).json(formattedFriends);
+    } catch (error) {
+      console.error('Ошибка при добавлении/удалении друга:', error);
+      res.status(500).json({ message: 'Ошибка сервера' });
+    }
+  };
