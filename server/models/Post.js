c7dbdaaddcc29f8a14bf20f62ad91901173df8ab
RuslanGon <ruslan441006@gmail.com>
2025-05-08 16:03:29 +0300

    wer

--- server/models/Post.js
+++ server/models/Post.js
@@ -1,9 +1,16 @@
 import mongoose from "mongoose";
 
 const postSchema = new mongoose.Schema({
+  userId: { type: String, required: true },
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
+  location: { type: String },
+  description: { type: String },
+  picturePath: { type: String},
+  userPicturePath: { type: String},
+  likes: { type: Map, of: Boolean },
+  comments: { type: Array, default: [] },
+
 }, 
 { timestamps: true}
 );
