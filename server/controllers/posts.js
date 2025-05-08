f25c85c0bf10cc0a149902e33e88d0dbf8ea202b
RuslanGon <ruslan441006@gmail.com>
2025-05-08 15:51:05 +0300

    wer

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -7,3 +7,27 @@ export const createPost = async (req, res) => {
         console.log(error);
     }
 }
+
+export const getFeedPost = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const getUserPosts = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const patchPost = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        console.log(error);
+    }
+}
--- server/routes/post.js
+++ server/routes/post.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import { verifyToken } from '../middleware/auth.js';
+import { getFeedPost, getUserPosts, patchPost } from '../controllers/posts.js';
 
 const router = express.Router()
 
 router.get('/', verifyToken, getFeedPost),
 router.get('/:userId/posts', verifyToken, getUserPosts)
+router.patch('/:id/like', verifyToken, patchPost)
 
 
 export default router
