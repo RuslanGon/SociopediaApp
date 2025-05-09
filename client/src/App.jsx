2e56f4f3115cf0841659a2d7f8d6cba2945ac564
RuslanGon <ruslan441006@gmail.com>
2025-05-09 18:43:49 +0300

    wer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,11 +4,19 @@ import { Route, Routes } from 'react-router-dom';
 import HomePage from './page/HomePage/HomePage.jsx';
 import LoginPage from './page/LoginPage/LoginPage.jsx';
 import ProfilePage from './page/ProfilePage/ProfilePage.jsx';
+import { useMemo } from 'react';
+import { useSelector } from 'react-redux';
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "./theme";
 
 
 const App = () => {
+  const mode = useSelector(state => state.auth.mode)
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode])
+  
   return (
-    <div>
+    <div className='app'>
       <Routes>
         <Route path='/' element={<LoginPage />} />
         <Route path='/home' element={<HomePage />} />
