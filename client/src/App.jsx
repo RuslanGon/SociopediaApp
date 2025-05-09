cdd451c8dc1a99b2baf3376fb061b9727e5de469
RuslanGon <ruslan441006@gmail.com>
2025-05-09 13:42:31 +0300

    wer

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css' 
+
+const App = () => {
+  return (
+    <div>App
+
+      <h2>wcwdc</h2>
+      <p>wcdwdc</p>
+    </div>
+  )
+}
+
+export default App
